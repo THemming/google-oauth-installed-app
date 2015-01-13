15112d2c22783b92339a51b890d32861c60f08f4
Tim Hemming <timhemming@gmail.com>
2015-01-13 22:00:16 +0000

    Removing deprecated code.

--- google-application-auth-http-client.js
+++
@@ -1,5 +0,0 @@
-
-exports.Client = require('./lib/client');
-exports.ApplicationAuth = require('./lib/application-auth');
-exports.AuthStore = require('./lib/auth-store');
-exports.receivers = require('./lib/receivers');
